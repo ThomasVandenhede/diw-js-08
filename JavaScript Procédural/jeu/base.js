2acc73c40628bdff2be797d4ec0261cc119f9d35
Thomas Vandenhede <thomasvandenhede@hotmail.com>
2018-04-05 10:29:27 +0200

    Refactor game by wrapping everything in IIFE

--- JavaScript Procédural/jeu/base.js
+++ JavaScript Procédural/jeu/base.js
@@ -1,108 +1,147 @@
-const MAX_NUMBER_OF_TURNS = 10;
+var game = (function() {
+  var _score = 0;
+  var _computerScore = 0;
+  var _turn = 0;
+  var _stats = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+  };
 
-var reponse;
-var score = 0;
-var computerScore = 0;
-var turn = 0;
+  var nombreAleatoire = function(min, max) {
+    var randomOutOf3 = Math.floor(Math.random() * (max - min + 1)) + min;
+    _stats[randomOutOf3]++;
+    return randomOutOf3;
+  }
+
+  var selectWinnerForThisTurn = function(userChoice, computerChoice) {
+    let text = '';
+    
+    if (userChoice.name === computerChoice.name) {
+      text = 'Draw';
+    } else {
+      const defeats = computerChoice.defeats.map((item, index) => {
+        return item.name;
+      });
+      
+      if (defeats.includes(userChoice.name)) {
+        text = computerChoice.defeats.find((item) => {
+          return (item.name === userChoice.name);
+        }).message;
+        _computerScore = _computerScore + 1;
+      } else {
+        text = choices[userChoice.name].defeats.find((item) => {
+          return (item.name === computerChoice.name);
+        }).message;
+        _score = _score + 1;
+      }
+    }
+    return text;
+  };
+
+  var resultat = function (txt) {
+    var monDiv = document.getElementById('resultat');
+    monDiv.innerHTML = txt;
+  };
+
+  var tour = function(reponse) {
+    var randomNumber = nombreAleatoire(0, Object.keys(choices).length - 1);
+    var computerChoice = choices[Object.keys(choices)[randomNumber]];
+    var userChoice = choices[reponse.toLowerCase()];
+  
+    text = selectWinnerForThisTurn(userChoice, computerChoice);
+    text = text + '<br />' + 'Vous ' + _score + ' - ' + _computerScore + ' Ordinateur';
+    
+    resultat(text);
+  };
+
+  return {
+    restart: function() {
+      state = {
+        score: 0,
+        computerScore: 0,
+        turn: 0,
+      }
+      resultat('');
+    },
+    repondre: function() {
+      var inputs = window.document.getElementsByName('reponse');
+      for (var i = 0; inputs[i]; i++) {
+        if (inputs[i].checked) {
+          reponse = inputs[i].value;
+        };
+      }
+      tour(reponse);
+    }
+  }
+})();
 
 const choices = {
   rock: {
     name: 'rock',
     defeats: [
-      'paper',
-      'lizard',
+      {
+        name: 'lizard',
+        message: 'rock crushes lizard',
+      },
+      {
+        name: 'scissors',
+        message: 'rock breaks scissors',
+      },
     ],
   },
   paper: {
     name: 'paper',
     defeats: [
-      'rock',
-      'spock',
+      {
+        name: 'rock',
+        message: 'paper covers rock',
+      },
+      {
+        name: 'spock',
+        message: 'paper disproves spock',
+      },
     ],
   },
   scissors: {
     name: 'scissors',
     defeats: [
-      'paper',
-      'lizard',
+      {
+        name: 'paper',
+        message: 'scissors cut paper',
+      },
+      {
+        name: 'lizard',
+        message: 'scissors decapitate lizard',
+      },
     ],
   },
   lizard: {
     name: 'lizard',
     defeats: [
-      'paper',
-      'spock',
+      {
+        name: 'paper',
+        message: 'lizard eats paper',
+      },
+      {
+        name: 'spock',
+        message: 'lizard poisons spock',
+      },
     ],
   },
   spock: {
     name: 'spock',
     defeats: [
-      'scissors',
-      'rock',
+      {
+        name: 'scissors',
+        message: 'spock smashes scissors',
+      },
+      {
+        name: 'rock',
+        message: 'spock vaporizes rock',
+      },
     ],
   },
 };
-
-var stats = {
-  1: 0,
-  2: 0,
-  3: 0
-};  
-
-var nombreAleatoire = function(min, max) {
-  var randomOutOf3 = Math.floor(Math.random() * (max - min + 1)) + min;
-  stats[randomOutOf3]++;
-  return randomOutOf3;
-}
-
-var repondre = function() {
-  var inputs = window.document.getElementsByName('reponse');
-  for (var i = 0; inputs[i]; i++) {
-    if (inputs[i].checked) {
-      reponse = inputs[i].value;
-    };
-  }
-  tour(reponse);
-};
-
-
-var resultat = function (txt) {
-  // window.document.getElementById('resultat').textContent = txt;
-  var monDiv = document.getElementById('resultat');
-  monDiv.innerHTML = txt;
-}
-
-var selectWinnerForThisTurn = function(userChoice, computerChoice) {
-  var text = '', textDefeat, textVictory, textDraw;
-  textDefeat = 'Vous perdez !';
-  textVictory = 'Vous gagnez !';
-  textDraw = "Égalité ! L'ordinateur a choisi comme vous";
-
-  if (userChoice.name === computerChoice.name) {
-    text = textDraw;
-  } else {
-    if (computerChoice.defeats.includes(userChoice.name)) {
-      text = textDefeat;
-      computerScore = computerScore + 1;
-    } else {
-      text = textVictory;
-      score = score + 1;
-    }
-  }
-  return text;
-}
-
-var tour = function(reponse) {
-  var randomNumber = nombreAleatoire(0, Object.keys(choices).length - 1);
-  var computerChoice = choices[Object.keys(choices)[randomNumber]];
-  var userChoice = choices[reponse.toLowerCase()];
-
-  console.log('user choice', userChoice);
-  console.log('computer choice', computerChoice);
-  var text = "L'ordinateur a choisi... " + computerChoice.name + " !<br />";
-
-  text = text + selectWinnerForThisTurn(userChoice, computerChoice);
-  
-  text = text + "<br />" + "Vous " + score + " - " +  computerScore + " Ordinateur";
-  resultat(text);
-}
